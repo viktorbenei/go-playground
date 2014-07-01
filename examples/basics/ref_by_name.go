f51952c4a80dc6f34fdc6344fd2dafeb4b9cabb6
Viktor Benei <viktor.benei@gmail.com>
2014-07-01 20:39:06 +0200

    ref by name: file comment and utility function Printfln

--- examples/basics/ref_by_name.go
+++ examples/basics/ref_by_name.go
@@ -1,3 +1,9 @@
+/*
+Reference by name
+
+A simple struct test, creating objects from structs with either positional parameters or through named parameters.
+*/
+
 package main
 
 import (
@@ -9,13 +15,25 @@ type Person struct {
 	age  int32
 }
 
+//
+// Base Println doesn't support the formatting Printf does
+//  but adding \n to every Printf call is not DRY (and not fun at all)
+// So this function does exactly this: you can use
+//  exactly the same way as you can Printf but it will
+//  automatically add a newline to the end of the line
+//
+func Printfln(format string, a ...interface{}) (n int, err error) {
+	format = format + "\n"
+	return fmt.Printf(format, a...)
+}
+
 func main() {
 	p := Person{"Viktor", 23}
 	p2 := Person{age: 21, name: "Zsu"}
-	fmt.Println("Person:", p)
-	fmt.Println("P2:", p2)
+	Printfln("Person: %#v", p)
+	Printfln("P2: %#v", p2)
 
 	// create with default values
 	p3 := new(Person)
-	fmt.Println("P3:", p3)
+	Printfln("P3: %v", p3)
 }
