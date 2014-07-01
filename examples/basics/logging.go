29e23c577c8a68b6c1a86a2b2f2e5fb68de8c430
Viktor Benei <viktor.benei@gmail.com>
2014-07-01 20:39:37 +0200

    logging examples, with log.Fatal test/demonstration

---
+++ examples/basics/logging.go
@@ -0,0 +1,31 @@
+/*
+Logging examples
+
+Simple logging examples
+*/
+
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+//
+// Base Println doesn't support the formatting Printf does
+//  but adding \n to every Printf call is not DRY (and not fun at all)
+// So this function does exactly this: you can use
+//  exactly the same way as you can Printf but it will
+//  automatically add a newline to the end of the line
+//
+func Printfln(format string, a ...interface{}) (n int, err error) {
+	format = format + "\n"
+	return fmt.Printf(format, a...)
+}
+
+func main() {
+	fmt.Println("Start")
+	Printfln("My format thing %d", 12)
+	log.Fatal("This will exit right away")
+	fmt.Println("This won't be logged")
+}
